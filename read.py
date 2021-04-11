6f611162ccc7f49e1cb5956c3d9200c1dbba435a
Sarah Wu <s58401021@yahoo.com.tw>
2021-04-11 15:14:01 +0800

    calc avg length of reviews

--- read.py
+++ read.py
@@ -6,7 +6,8 @@ with open('reviews.txt', 'r', encoding="utf-8") as f:
 		count += 1
 		if count % 2 == 0:
 		    print(len(data))
-print(len(data))
-print(data[0])
-print('------------------------')
-print(data[1])
+print('檔案讀取完了, 總共有', len(data), '筆資料')
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d) # len(d) = 所有留言的長度
+print('留言的平均長度為', sum_len/len(data))
